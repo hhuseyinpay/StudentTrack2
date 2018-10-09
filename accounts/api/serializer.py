168cd30c9090867fe371cb943b66a37611e35dc9
hhuseyinpay <hhuseyinpay@gmail.com>
2018-10-09 18:48:29 +0300

    update

--- accounts/api/serializer.py
+++ accounts/api/serializer.py
@@ -7,7 +7,7 @@ class UserModelSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=30)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=30)
-    password = serializers.CharField(write_only=True, required=False)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
@@ -153,12 +153,9 @@ class AdminProfileCreateSerializer(AdminProfileModelSerializer):
         return pr
 
 
-class AdminProfileUpdateSerializer(AdminProfileModelSerializer):
+class AdminProfileUpdateSerializer(AdminProfileCreateSerializer):
     user = UserModelSerializer(required=False)
-    classroom = serializers.PrimaryKeyRelatedField(required=False, queryset=ClassRoom.objects.all())
-    related_area = serializers.PrimaryKeyRelatedField(required=False, queryset=Area.objects.all())
     related_region = serializers.PrimaryKeyRelatedField(required=False, queryset=Region.objects.all())
-    group = serializers.PrimaryKeyRelatedField(required=False, queryset=Groups.objects.all())
 
     def update(self, instance, validated_data):
         instance.is_student = validated_data.get('is_student', instance.is_student)
