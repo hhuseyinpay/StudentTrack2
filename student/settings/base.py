7a6f43b68209db2c88cb44a44aad05fbb9636c78
hhuseyinpay <hhuseyinpay@gmail.com>
2018-10-12 14:40:46 +0300

    logging enabled

--- student/settings/base.py
+++ student/settings/base.py
@@ -165,3 +165,46 @@ DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 REPORT_DAILYSTUDY_DIRECTORY = MEDIA_ROOT + '/reports/excel/daily_study/'
 REPORT_TEMPLATE_DIRECTORY = MEDIA_ROOT + '/reports/template/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+            # But the emails are plain text by default - HTML is nicer
+            'include_html': True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/django/student.log'
+        },
+        'logfile_warning': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/django/student_warning.log'
+        },
+    },
+    'loggers': {
+        # Again, default Django configuration to email unhandled exceptions
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['logfile_warning'],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+    },
+}
