49aafec67bf477a7bcc8cd66d2abccd3eb2a3282
hhuseyinpay <hhuseyinpay@gmail.com>
2018-10-16 09:41:58 +0300

    caneditprofile and caneditclassroom added

--- accounts/api/permissions.py
+++ accounts/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from ..models import ClassRoom, Area, Region
 
 
 class IsTeacher(permissions.BasePermission):
@@ -64,5 +65,28 @@ def is_authority(authority_user, low_authority_profile):
 
 class CanEditProfile(IsTeacherExecutiveAdmin):
     def has_object_permission(self, request, view, obj):
-        user = request.user
-        return is_authority(user, obj)
+        admin_user = request.user
+        if admin_user == obj.user:
+            return True
+        if admin_user.profile.is_teacher and obj.is_student:
+            if obj.classroom in ClassRoom.objects.filter(teachers=admin_user):
+                return True
+        if admin_user.profile.is_executive and not obj.is_executive and not obj.is_admin:
+            if obj.related_area in Area.objects.filter(executives=admin_user):
+                return True
+        elif admin_user.profile.is_admin and not obj.is_admin:
+            if obj.related_region in Region.objects.filter(admins=admin_user):
+                return True
+        return False
+
+
+class CanEditClassroom(IsExecutiveAdmin):
+    def has_object_permission(self, request, view, obj):
+        admin_user = request.user
+        if admin_user.profile.is_executive and admin_user in obj.related_area.executives.all():
+            return True
+        if admin_user.profile.is_admin and admin_user.profile.related_region == obj.related_area.related_region:
+            return True
+        if admin_user in obj.teachers.all():
+            return True
+        return False
