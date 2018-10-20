c28cb7d8c7ba32fbab28785e402d4608e3bbbf9a
hhuseyinpay <hhuseyinpay@gmail.com>
2018-10-20 18:06:42 +0300

    drf_yasg added

--- student/settings/base.py
+++ student/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'cachalot',
+    'drf_yasg',
 
     'student',
     'apps.base',
@@ -116,7 +117,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
