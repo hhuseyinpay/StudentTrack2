6d1f6f1cef4d98c2bda1561f7b8cd3eeb947da30
hhuseyinpay <hhuseyinpay@gmail.com>
2018-11-28 23:25:03 +0300

    add: beat multipart

--- deploy/run_celery.sh
+++ deploy/run_celery.sh
@@ -2,5 +2,5 @@
 
 # multi start w1
 celery multi start w1 -A student.settings.celery:app worker -l INFO --pidfile= &
-celery -A student.settings.celery:app beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --pidfile= &
+celery multi start w1 -A student.settings.celery:app beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --pidfile= &
 celery flower -A student.settings.celery:app --broker=redis://redis:6379/ -l INFO --pidfile=
