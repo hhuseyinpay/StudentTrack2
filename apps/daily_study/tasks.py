7b34fbd5192b5ae91afa7615e8c3b10bde620325
hhuseyinpay <hhuseyinpay@gmail.com>
2018-11-28 21:55:45 +0300

    add: missing daily studies will automatically fill

---
+++ apps/daily_study/tasks.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+
+from django.utils.timezone import now
+from celery import shared_task
+
+from account.models import User
+from .models import DailyStudy, Study
+
+
+@shared_task
+def fill_missing_daily_study():
+    yesterday = (now() - timedelta(days=1)).date()
+    students = User.objects \
+        .filter(user_type=User.STUDENT) \
+        .exclude(dailystudy__created_day=yesterday).only('id', 'course_group') \
+        .prefetch_related('course_group__courses')
+
+    for u in students:
+        ds = DailyStudy.objects.create(user=u, created_day=yesterday)
+
+        studies = []
+        for course in u.course_group.courses.all():
+            studies.append(Study(daily_study=ds, course=course, begining=0, end=0, amount=0))
+        Study.objects.bulk_create(studies)
