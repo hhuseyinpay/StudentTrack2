2260f3332870a8c19a6ad5d69cf5076636c638f5
hhuseyinpay <hhuseyinpay@gmail.com>
2018-12-09 22:22:24 +0300

    update: rm unnecessary

--- deploy/run_celery.sh
+++ deploy/run_celery.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-rm ./*.pid
-
 # multi start w1
-celery multi start w2 -A student.settings.celery:app worker -l INFO --pidfile="./%n.pid" &
-celery multi start w2 -A student.settings.celery:app beat -l INFO --scheduler=django_celery_beat.schedulers:DatabaseScheduler --pidfile="./%nB.pid" &
-celery flower -A student.settings.celery:app --broker=redis://redis:6379/ -l INFO --pidfile="./%nF.pid"
+celery  -A student.settings.celery:app worker -l INFO --pidfile="%n.pid" &
+celery  -A student.settings.celery:app beat -l INFO --scheduler=django_celery_beat.schedulers:DatabaseScheduler --pidfile="%nB.pid" &
+celery flower -A student.settings.celery:app --broker=redis://redis:6379/ -l INFO --pidfile="%nF.pid"
