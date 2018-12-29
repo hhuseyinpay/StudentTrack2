ff4ada7a208fb13908f134e587fef4f9bcbf46f6
hhuseyinpay <hhuseyinpay@gmail.com>
2018-12-29 23:53:08 +0300

    update

--- apps/syllabus/admin.py
+++ apps/syllabus/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import UserSyllabus, Content, Syllabus
 
-admin.site.register(Content)
-admin.site.register(UserSyllabus)
-
 
 @admin.register(Syllabus)
-class MahalleAdmin(admin.ModelAdmin):
+class SyllabusAdmin(admin.ModelAdmin):
     filter_horizontal = ('contents',)
+
+
+admin.site.register(Content)
+admin.site.register(UserSyllabus)
