4411e25574c4f207ee042aeb2bc033ddeb0df08a
hhuseyinpay <hhuseyinpay@gmail.com>
2018-12-29 23:29:01 +0300

    add: Syllabus'a horizontalfield eklendi

--- apps/syllabus/admin.py
+++ apps/syllabus/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import UserSyllabus, Content, Syllabus
 
-admin.site.register(Syllabus)
 admin.site.register(Content)
 admin.site.register(UserSyllabus)
+
+
+@admin.register(Syllabus)
+class MahalleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('contents',)
