58113436528df3d4c6471292529dd3dc16c9fa09
hhuseyinpay <hhuseyinpay@gmail.com>
2019-01-02 15:22:54 +0300

    update

--- student/settings/base.py
+++ student/settings/base.py
@@ -3,6 +3,8 @@ import sys
 
 from celery.schedules import crontab
 
+DEBUG=False
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
