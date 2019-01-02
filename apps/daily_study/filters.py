c6c66b52a74e4c9cb9d37cd1ad1e5b6df5b200fc
hhuseyinpay <hhuseyinpay@gmail.com>
2019-01-02 14:59:15 +0300

    bugfix: is_validated filter return all unvalidated ds. But we want to see student update the ds and after ds drop in the validate screen.

--- apps/daily_study/filters.py
+++ apps/daily_study/filters.py
@@ -1,5 +1,5 @@
+from django_filters import DateFilter, BooleanFilter
 from django_filters.rest_framework import FilterSet
-from django_filters import DateFilter
 
 from daily_study.models import DailyStudy
 
@@ -7,6 +7,22 @@ from daily_study.models import DailyStudy
 class DailyStudyCreatedDayFilter(FilterSet):
     begining = DateFilter(field_name='created_day', lookup_expr='gte')
     end = DateFilter(field_name='created_day', lookup_expr='lte')
+    is_validated = BooleanFilter(method='is_validated_filter')
+
+    def is_validated_filter(self, queryset, name, value):
+        """
+        student dailystudyleri gece otomatik oluşturuluyor ve is_validated false oluyor.
+        Ancak student dailystudy'i daha doldurmadı ve bütün studyler default oalrak 0 geldi.
+
+        dailystudy onay ekranında görünen daily studylerin student tarafından doldurulduğunu tespit etmek için
+        updated__isnull kontrolü ekledim.
+        Otomatik eklenen dailystudyler student tarafından update edildikten sonra onay ekranına düşecek..
+        """
+        if value == False:
+
+            return queryset.filter(updated__isnull=False, is_validated=False)
+        else:
+            return queryset
 
     class Meta:
         model = DailyStudy
